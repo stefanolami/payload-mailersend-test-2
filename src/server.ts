683cbf34f109b0645b5506329911f743a747a89a
stefanolami <stefanolami90@gmail.com>
2024-04-04 10:47:44 +0200

    added email config

--- src/server.ts
+++ src/server.ts
@@ -6,22 +6,36 @@ const app = express()
 
 // Redirect root to Admin panel
 app.get('/', (_, res) => {
-  res.redirect('/admin')
+	res.redirect('/admin')
 })
 
 const start = async () => {
-  // Initialize Payload
-  await payload.init({
-    secret: process.env.PAYLOAD_SECRET,
-    express: app,
-    onInit: async () => {
-      payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
-    },
-  })
+	// Initialize Payload
+	await payload.init({
+		secret: process.env.PAYLOAD_SECRET,
+		email: {
+			transportOptions: {
+				host: process.env.SMTP_HOST,
+				auth: {
+					user: process.env.SMTP_USER,
+					pass: process.env.SMTP_PASS,
+				},
+				port: Number(process.env.SMTP_HOST),
+				secure: Number(process.env.SMTP_PORT) === 465, // true for port 465, false (the default) for 587 and others
+				requireTLS: true,
+			},
+			fromName: 'hello',
+			fromAddress: 'hello@example.com',
+		},
+		express: app,
+		onInit: async () => {
+			payload.logger.info(`Payload Admin URL: ${payload.getAdminURL()}`)
+		},
+	})
 
-  // Add your own express routes here
+	// Add your own express routes here
 
-  app.listen(3000)
+	app.listen(3000)
 }
 
 start()
